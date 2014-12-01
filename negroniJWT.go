67c2c7e113c80898f06dcb18d12ac67feffaecde
unknown <kyle.dennison@gmail.com>
2014-11-30 21:43:06 -0800

    Initial commit of source

---
+++ negroniJWT.go
@@ -0,0 +1,97 @@
+package negroniJWT
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	//"fmt"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/context"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const (
+	CONTEXT_KEY = "jwt"
+)
+
+var (
+	once              sync.Once
+	privKeyPEMEncoded []byte
+	pubKeyPEMEncoded  []byte
+	failRequest       bool
+)
+
+func Init(failRequest bool) {
+	failRequest = failRequest
+	once.Do(generateKeys)
+}
+
+func generateKeys() {
+	privKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	privKeyPEMEncoded = pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE CONTEXT_KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privKey),
+	})
+	pubANS1, err := x509.MarshalPKIXPublicKey(&privKey.PublicKey)
+	if err != nil {
+		panic(err)
+	}
+	pubKeyPEMEncoded = pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC CONTEXT_KEY",
+		Bytes: pubANS1,
+	})
+}
+
+func getClaims(token string) (claims map[string]interface{}, err error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return pubKeyPEMEncoded, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if parsedToken.Valid {
+		return parsedToken.Claims, nil
+	}
+	return nil, errors.New("Token is invalid")
+}
+
+// Middleware the main middleware function. Use with negroni.HandlerFunc to apply middleware like so :
+// n := negroni.Classic()
+// n.Use(negroni.HandlerFunc(negroniJWT.Middleware))
+func Middleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	var authToken string = r.Header.Get("Authorization")
+	claims, err := getClaims(authToken)
+	if err == nil || failRequest == false {
+		if err == nil {
+			context.Set(r, CONTEXT_KEY, claims)
+		}
+		next(rw, r)
+	} else {
+		rw.WriteHeader(401)
+	}
+}
+
+// GenerateToken generates the base64 encoded JSON Web Token including the claims map provided and and expiration time.
+func GenerateToken(claims map[string]interface{}, expiration time.Time) (s string, err error) {
+	t := jwt.New(jwt.GetSigningMethod("RS256"))
+	t.Claims = claims
+	t.Claims["exp"] = expiration.Unix()
+	return t.SignedString(privKeyPEMEncoded)
+}
+
+// Get attempts to retrieve the claims map for request. If there was an error decoding the JSON Web Token.
+func Get(r *http.Request) (claims map[string]interface{}, ok bool) {
+	c, ok := context.GetOk(r, CONTEXT_KEY)
+	if !ok {
+		return claims, ok
+	}
+	claims, castOk := c.(map[string]interface{})
+	return claims, castOk
+}
---
+++ negroniJWT_test.go
@@ -0,0 +1,147 @@
+package negroniJWT
+
+import (
+	"bytes"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+)
+
+const (
+	user = "Bob"
+)
+
+func startTestServer(failRequest bool, t *testing.T, loginFunc, validToken, invalidToken func(http.ResponseWriter, *http.Request)) {
+	Init(failRequest)
+	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(Middleware))
+	m := mux.NewRouter()
+	m.HandleFunc("/login", loginFunc).Methods("POST")
+	m.HandleFunc("/validToken", validToken).Methods("GET")
+	m.HandleFunc("/invalidToken", invalidToken).Methods("GET")
+	n.UseHandler(m)
+	go http.ListenAndServe(":3333", n)
+}
+
+func TestValidClaims(t *testing.T) {
+	loginFunc := func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(b, []byte(user)) {
+			return
+		}
+		claims := make(map[string]interface{})
+		claims["Username"] = user
+		// generate JWT token with encrypted claims
+		token, err := GenerateToken(claims, time.Now().Add(1*time.Second))
+		if err != nil {
+			t.Fatal(err)
+		}
+		w.Write([]byte(token))
+	}
+
+	validToken := func(w http.ResponseWriter, r *http.Request) {
+		claims, ok := Get(r)
+		if !ok {
+			t.Fatal("Claims retrieval shows not ok.")
+			w.WriteHeader(401)
+			return
+		}
+		username, ok := claims["Username"]
+		if !ok {
+			t.Fatal("Claims do not contain a key called Username")
+			w.WriteHeader(401)
+			return
+		}
+
+		usernameString, ok := username.(string)
+		if !ok {
+			t.Fatal("Claim username is not a string")
+			w.WriteHeader(401)
+			return
+		}
+
+		if usernameString != user {
+			t.Fatal("Claim username is not a string 'Bob'")
+			w.WriteHeader(401)
+			return
+		}
+
+		w.Write([]byte("Hello Bob"))
+	}
+
+	invalidToken := func(w http.ResponseWriter, r *http.Request) {
+		_, ok := Get(r)
+		if ok {
+			t.Fatal("expected ok to be falsy")
+		} else {
+			w.WriteHeader(401)
+			w.Write([]byte("Permission Denied"))
+			return
+		}
+
+		w.Write([]byte("Hello Bob"))
+	}
+
+	startTestServer(false, t, loginFunc, validToken, invalidToken)
+	client := &http.Client{}
+
+	// retrieve token
+	resp, err := http.Post("http://localhost:3333/login", "text/plain", bytes.NewBufferString("Bob"))
+	if err != nil {
+		t.Fatal("Error logging in, ", err.Error())
+	}
+	token, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Error Reading login response, ", err.Error())
+	}
+
+	// test case where token is valid
+	req, err := http.NewRequest("GET", "http://localhost:3333/validToken", nil)
+	if err != nil {
+		t.Fatal("Error generating request, ", err.Error())
+	}
+	req.Header.Add("Authorization", string(token))
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Error doing GET to /validToken, ", err.Error())
+	}
+	if resp.StatusCode != 200 {
+		t.Fatal("Expected status code of 200, got ", resp.Status)
+	}
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Error Reading /validToken response, ", err.Error())
+	}
+	if !bytes.Equal(response, []byte("Hello Bob")) {
+		t.Fatal("Expected 'Hello Bob' response, got ", string(response))
+	}
+
+	// test case where token is invalid
+	req, err = http.NewRequest("GET", "http://localhost:3333/invalidToken", nil)
+	if err != nil {
+		t.Fatal("Error generating request, ", err.Error())
+	}
+	req.Header.Add("Authorization", string(token[1:]))
+	resp, err = client.Do(req)
+	if resp.StatusCode != 401 {
+		t.Fatal("Expected error code of 401, got ", resp.Status)
+	}
+
+	// test case where token is expired
+	time.Sleep(2 * time.Second)
+	req, err = http.NewRequest("GET", "http://localhost:3333/invalidToken", nil)
+	if err != nil {
+		t.Fatal("Error generating request, ", err.Error())
+	}
+	req.Header.Add("Authorization", string(token))
+	resp, err = client.Do(req)
+	if resp.StatusCode != 401 {
+		t.Fatal("Expected error code of 401, got ", resp.Status)
+	}
+}
